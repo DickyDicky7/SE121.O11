2126383ead656cb64c148287e92cefb6532a11ac
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-09-14 09:42:03 +0700

    Add project files.

---
+++ WorkScheduleReminder.MAUIBlazor/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WorkScheduleReminder.MAUIBlazor
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
