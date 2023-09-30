fe774130fdf72b34596a577f4f249126a8c9c4cb
ThienAn <21521812@gm.uit.edu.vn>
2023-09-30 18:45:24 +0700

    push1

---
+++ WorkScheduleReminder.SharedBlazorComponents/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkScheduleReminder.SharedBlazorComponents
+{
+	public class Helper
+	{
+		public static Assembly[] LibraryAdditionalAssemblies = new[] { Assembly.GetExecutingAssembly()};
+		public static string GetResource(string path)
+		{
+			StringBuilder stringBuilder = new("_content/WorkScheduleReminder.SharedBlazorComponents");
+			return stringBuilder.Append(path).ToString();
+		}
+	}
+}
