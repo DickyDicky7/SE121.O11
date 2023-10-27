b70cdfede8e7def8b2cd7ba377951422d1e8045c
DickyDicky7 <87304213+DickyDicky7@users.noreply.github.com>
2023-10-28 00:41:59 +0700

    UI #001 (#1)
    * push1
    
    * split component
    
    * adjust component's name
    
    * reformat code #1
    
    * reformat code #2
    
    * prepare for pull
    
    * AddTask component #1
    
    * desktopUI
    
    ---------
    
    Co-authored-by: ThienAn <21521812@gm.uit.edu.vn>

---
+++ WorkScheduleReminder.SharedBlazorComponents/Helper.cs
@@ -0,0 +1,59 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Forms;
+namespace WorkScheduleReminder.SharedBlazorComponents
+{
+	public class Helper
+	{
+		public enum ViewType
+		{
+			DefaultView, CustomView, WorkspaceView
+		}
+		public static Assembly[] DependencyAdditionalAssemblies = new[] { Assembly.GetExecutingAssembly()};
+		public static string GetResource(string path)
+		{
+			StringBuilder stringBuilder = new("_content/WorkScheduleReminder.SharedBlazorComponents");
+			return        stringBuilder.Append(path).ToString();
+		}
+
+		public enum DeviceType
+		{
+			Desktop, Mobile, Unknown
+		}
+
+		public static Dictionary<byte, MudTheme> CustomThemes { get; } = new()
+		{
+			{
+				1
+				,
+				new()
+				{
+					
+				}
+			}
+			,
+			{
+				2
+				,
+				new()
+				{
+
+				}
+			}
+			,
+			{
+				3,
+				new()
+				{
+
+				}
+			}
+			,
+		};
+	}
+}
