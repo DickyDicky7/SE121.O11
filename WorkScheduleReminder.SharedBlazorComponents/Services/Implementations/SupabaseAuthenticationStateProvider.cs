e10decc4ef4ed5909a58ac86bda290109ebec1cc
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-10-28 00:00:13 +0700

    code frontend auth #01

---
+++ WorkScheduleReminder.SharedBlazorComponents/Services/Implementations/SupabaseAuthenticationStateProvider.cs
@@ -0,0 +1,37 @@
+﻿using System.Security
+	 .Claims;
+using Microsoft.AspNetCore
+			   .Components.Authorization;
+
+namespace WorkScheduleReminder.SharedBlazorComponents.Services.Implementations
+{
+	public class SupabaseAuthenticationStateProviderService : AuthenticationStateProvider
+	{
+	public       SupabaseAuthenticationStateProviderService
+			    (Supabase
+		.Client  supabaseClient) : base()
+		{
+			this.supabaseClient  = supabaseClient;
+		}
+
+		private readonly Supabase.Client supabaseClient;
+
+		public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			ClaimsPrincipal claimsPrincipal = new();
+			if (supabaseClient.Auth.CurrentSession != null)
+			{
+				string? UserId  = supabaseClient.Auth.CurrentUser?.Id;
+				if     (UserId != null)
+				{
+					claimsPrincipal = new(new ClaimsIdentity(new Claim[]
+					{ new(nameof(UserId), UserId)
+					}
+					, authenticationType: "SUPABASE_GOTRUE"));
+				}
+			}
+			return new
+				   (claimsPrincipal);
+		}
+	}
+}
