a467e63f63aad0ec933e617ee23f715e9a06d905
ZeVnan <thienan27103@gmail.com>
2023-10-29 21:52:55 +0700

    UI login and signup

--- WorkScheduleReminder.SharedBlazorComponents/Helper.cs
+++ WorkScheduleReminder.SharedBlazorComponents/Helper.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
 namespace WorkScheduleReminder.SharedBlazorComponents
 {
 	public class Helper
