98e1859b72ffbc43eaceebf8c5b35d5ff7cc494e
ZeVnan <thienan27103@gmail.com>
2023-11-03 16:48:24 +0700

    create forgot password component

--- WorkScheduleReminder.MAUIBlazor/App.xaml.cs
+++ WorkScheduleReminder.MAUIBlazor/App.xaml.cs
@@ -8,5 +8,12 @@
 
 			MainPage = new MainPage();
 		}
+		protected override Window CreateWindow(IActivationState activationState)
+		{
+			Window window = base.CreateWindow(activationState);
+			window.MinimumHeight = 400;
+			window.MinimumWidth = 1000;
+			return window;
+		}
 	}
 }
