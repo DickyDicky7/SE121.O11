f154526c789e730eee472f0550764d4b062bdb7a
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-11-12 10:35:46 +0700

    forgot password @02

---
+++ WorkScheduleReminder.StaticPages/redirect-recovery.js
@@ -0,0 +1,13 @@
+if (window.location.hash
+||  window.location.search)
+{
+    const currentPageURLSearchParams = new
+                     URLSearchParams(window.location.search);
+    history.replaceState({ recoveryCode: currentPageURLSearchParams.get("code"), }, "", window.location.pathname);
+}
+else
+{
+    document
+        .querySelector(".container")
+        .innerHTML = `Your recovery code: ${history.state.recoveryCode}`;
+}
