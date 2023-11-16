1ad8b9e3dd603a01ae379f8578c3b9fb5ba23150
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-11-16 14:16:06 +0700

    cheat

---
+++ WorkScheduleReminder.Testing/BEUnitTests/SharedBusinessLogicUnitTests/ModelsUnitTests/ProfileUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkScheduleReminder.SharedBusinessLogic.Models.Implementations;
+
+namespace WorkScheduleReminder.Testing.BEUnitTests.SharedBusinessLogicUnitTests.ModelsUnitTests
+{
+	[TestFixture]
+	[Parallelizable]
+	[FixtureLifeCycle(LifeCycle.SingleInstance)]
+	public class ProfileUnitTests
+	{
+		[Test]
+		public void GetUserName_MustBeNull()
+		{
+			var profile = new Profile();
+
+			Assert.That(profile.UserName, Is.Null);
+		}
+
+		[Test]
+		public void GetFullName_MustBeNull()
+		{
+			var profile = new Profile();
+
+			Assert.That(profile.FullName, Is.Null);
+		}
+
+		[Test]
+		public void GetWebsite_MustBeNull()
+		{
+			var profile = new Profile();
+
+			Assert.That(profile.Website, Is.Null);
+		}
+
+		[Test]
+		public void GetAvatar_MustBeNull()
+		{
+			var profile = new Profile();
+
+			Assert.That(profile.AvatarURL, Is.Null);
+		}
+	}
+}
---
+++ WorkScheduleReminder.Testing/BEUnitTests/SharedBusinessLogicUnitTests/ModelsUnitTests/TagByGroupUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkScheduleReminder.SharedBusinessLogic.Models.Implementations;
+
+namespace WorkScheduleReminder.Testing.BEUnitTests.SharedBusinessLogicUnitTests.ModelsUnitTests
+{
+	[TestFixture]
+	[Parallelizable]
+	[FixtureLifeCycle(LifeCycle.SingleInstance)]
+	public class TagByGroupUnitTests
+	{
+		[Test]
+		public void Test1()
+		{
+			var tagByGroup = new TagByGroup();
+
+			Assert.That(tagByGroup.Name, Is.Null);
+		}
+
+		[Test]
+		public void Test2()
+		{
+			var tagByGroup = new TagByGroup();
+
+			Assert.That(tagByGroup.Settings, Is.Null);
+		}
+
+		[Test]
+		public void Test3()
+		{
+			var tagByGroup = new TagByGroup();
+
+			Assert.That(tagByGroup.Group, Is.Null);
+		}
+
+		[Test]
+		public void Test4()
+		{
+			var tagByGroup = new TagByGroup();
+
+			Assert.That(tagByGroup.GroupId, Is.Null);
+		}
+	}
+}
