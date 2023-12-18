b18f2c329060929d0be44a5281befcdf4eec024f
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-12-19 00:56:35 +0700

    actions with task ~02

--- WorkScheduleReminder.SharedBlazorComponents/Common.cs
+++ WorkScheduleReminder.SharedBlazorComponents/Common.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WorkScheduleReminder.SharedBlazorComponents.Services.Implementations;
+using Models = WorkScheduleReminder.SharedBusinessLogic.Models.Implementations;
 
 namespace WorkScheduleReminder.SharedBlazorComponents
 {
 	public static class Common
 	{
-		public record ReminderRecurringSettings
+		public record class ReminderRecurringSettings
 		{
 			public List<string> DaysOfWeek { get; set; } = new();
 			public int Every { get; set; } = 1;
@@ -20,81 +20,30 @@ namespace WorkScheduleReminder.SharedBlazorComponents
 			=> new string[7] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
 		}
 
-		//public static async Task<bool> LogIn(
-		//	SupabaseAuthenticationStateProviderService
-		//	supabaseAuthenticationStateProviderService,
-		//	string email   ,
-		//	string password, ISnackbar snackbar)
-		//{
-		//	(bool ok1, string reason1) = Helper.CheckEmailFormat(email);
-		//	if  (!ok1)
-		//	{
-		//		snackbar.Add(Message.Error.CANNOT_LOG_IN_(reason1),
-		//		Severity.Error);
-		//		return false;
-		//	}
-		//	(bool ok2, string reason2) = Helper.CheckPasswordFormat(password);
-		//	if  (!ok2)
-		//	{
-		//		snackbar.Add(Message.Error.CANNOT_LOG_IN_(reason2),
-		//		Severity.Error);
-		//		return false;
-		//	}
-		//	(bool ok3, string reason3) = await supabaseAuthenticationStateProviderService.SignIn_(email, password);
-		//	if  (!ok3)
-		//	{
-		//		snackbar.Add(Message.Error.CANNOT_LOG_IN_(reason3),
-		//		Severity.Error);
-		//		return false;
-		//	}
-		//	else
-		//	{
-		//		snackbar.Add(reason3,
-		//		Severity.Success);
-		//		return true;
-		//	}
-		//}
+		public record class Tag
+		{
+			public Guid Id { get; set; }
+			public Color Color { get; set; }
+			public string Name { get; set; } = null!;
+		}
 
-		//public static async Task<bool> SignUp(
-		//	SupabaseAuthenticationStateProviderService
-		//	supabaseAuthenticationStateProviderService,
-		//	string email            ,
-		//	string password         ,
-		//	string passwordConfirmed, ISnackbar snackbar)
-		//{
-		//	(bool ok1, string reason1) = Helper.CheckEmailFormat(email);
-		//	if  (!ok1)
-		//	{
-		//		snackbar.Add(Message.Error.CANNOT_SIGN_UP(reason1),
-		//		Severity.Error);
-		//		return false;
-		//	}
-		//	(bool ok2, string reason2) = Helper.CheckPasswordFormat(password);
-		//	if  (!ok2)
-		//	{
-		//		snackbar.Add(Message.Error.CANNOT_SIGN_UP(reason2),
-		//		Severity.Error);
-		//		return false;
-		//	}
-		//	if  (password != passwordConfirmed)
-		//	{
-		//		snackbar.Add(Message.Error.PASSWORD_AND_PASSWORD_CONFIRMED_NOT_MATCH,
-		//		Severity.Error);
-		//		return false;
-		//	}
-		//	(bool ok4, string reason4) = await supabaseAuthenticationStateProviderService.SignUp_(email, password);
-		//	if  (!ok4)
-		//	{
-		//		snackbar.Add(Message.Error.CANNOT_SIGN_UP(reason4),
-		//		Severity.Error);
-		//		return false;
-		//	}
-		//	else
-		//	{
-		//		snackbar.Add(reason4,
-		//		Severity.Success);
-		//		return true;
-		//	}
-		//}
+		public record class SectionTask
+		{
+			public Models.Task Task { get; init; } = null!;
+			public List<Tag>   Tags { get; init; } = null!;
+			public bool    SubtasksAvailable { get; init; } = false;
+			public bool AttachmentsAvailable { get; init; } = false;
+			public List<Models.Profile> Assignees { get; set; } = null!;
+		}
+
+		public record class Section
+		{
+			public bool IsSectionByBoard                { get; init; } = false;
+			public Models.SectionByBoard SectionByBoard { get; init; } = null!;
+			public int DaysOffset { get; set; }
+			public string Name    { get; set; } = null!;
+			public string NewTask { get; set; } = string.Empty;
+			public List<SectionTask> SectionTasks { get;  init; } = null!;
+		}
 	}
 }
